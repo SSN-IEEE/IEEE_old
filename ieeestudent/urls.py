3bdf85c00dc3d0ecc59986dde6c6d31e277d010e
Chandramowli J <chamowstudios@outlook.com>
2017-05-01 21:04:18 +0530

    Fix Bugs

--- ieeestudent/urls.py
+++ ieeestudent/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name='ieeestudent'
 
 urlpatterns = [
-    url(r'^home/$', views.index, name='index'),
-    url(r'^home/events/2015/$', views.events_2015, name='events_2015'),
-    url(r'^home/events/2016/$', views.events_2016, name='events_2016'),
-    url(r'^home/events/2017/$', views.events_2017, name='events_2017'),
-    url(r'^home/events/$', views.events, name='events'),
-    url(r'^home/gallery/$', views.gallery, name='gallery'),
-    url(r'^home/about/$', views.about, name='about')
+    url(r'^$', views.index, name='index'),
+    url(r'^events/2015/$', views.events_2015, name='events_2015'),
+    url(r'^events/2016/$', views.events_2016, name='events_2016'),
+    url(r'^events/2017/$', views.events_2017, name='events_2017'),
+    url(r'^events/$', views.events, name='events'),
+    url(r'^gallery/$', views.gallery, name='gallery'),
+    url(r'^about/$', views.about, name='about')
 ]
